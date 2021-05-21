dc43a666ac635becb9986cdb82c15487b1859f79
TLRKiliann <philogenie@protonmail.com>
2021-05-21 17:29:40 +0200

    modif

---
+++ ana_py3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+    To get public IP
+"""
+
+from requests import get
+import sys
+import os
+
+#os.system("xfce4-terminal -e 'bash -c \"netstat -tpe; exec bash\"'")
+
+url = "http://icanhazip.com"
+print("CHECK public IP from : {}".format(url))
+
+r = get(url).text
+print(r)
+
+print("your IP Address is : {}".format(r))
