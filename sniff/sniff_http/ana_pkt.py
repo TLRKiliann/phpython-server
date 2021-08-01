108eb980dfb9bfef110bb998f39b70d82852e0b8
TLRKiliann <philogenie@protonmail.com>
2021-08-01 09:51:09 +0200

    improved script

--- sniff/sniff_http/ana_pkt.py
+++ sniff/sniff_http/ana_pkt.py
@@ -17,13 +17,21 @@ print(__Analyzer__)
 as_ip = input("\nEnter IP of ethernet interface to sniff : ")
 
 # To get addr source and destination
-class Unpack():
+class unpack():
     def __cinit__(self):
         self.data = None
 
     # Ethernet Header
     def eth_header(self, data):
         storeobj = data
+        storeobj = struct.unpack("!6s6sH", storeobj)
+        destination_mac = binascii.hexlify(storeobj[0])
+        source_mac = binascii.hexlify(storeobj[1])
+        eth_protocol = storeobj[2]
+        data = {"Destination Mac":destination_mac,
+        "Source Mac":source_mac,
+        "Protocol":eth_protocol}
+        return data
 
     # IP Header Extraction
     def ip_header(self, data):
@@ -52,20 +60,18 @@ class Unpack():
         return data
 
 if os.name == "nt":
-    #For Windows
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
     # bind is for listening sockets...
     s.bind((as_ip, 0))
     s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
     s.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 else:
-    # For Linux
     s = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0800))
 
 while True:
     print("<<<<<<<<<<<<< HEAD")
     pkt = s.recvfrom(65565)
-    dpack = Unpack()
+    dpack = unpack()
     print("\n===>> [+] ------ IP Header ------ [+]")
     for i in dpack.ip_header(pkt[0][14:34]).items():
         a, b = i
