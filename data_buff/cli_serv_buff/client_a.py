cc284186fef6af3d2a685a12cde4c5695a52fb0f
TLRKiliann <philogenie@protonmail.com>
2021-08-01 02:13:21 +0200

    cli_serv improved for mem_buff

--- data_buff/cli_serv_buff/client_a.py
+++ data_buff/cli_serv_buff/client_a.py
@@ -8,20 +8,20 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1235))
 
 while True:
-	msg = ""
-	new_msg = True
-	while True:
-		msg = s.recv(16)
-		if new_msg:
-			print(f"New message lenght : {msg[:HEADERSIZE]}")
-			msg_len = int(msg[:HEADERSIZE])
-			new_msg = False
+    full_msg = ""
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print(f"New message length : {msg[:HEADERSIZE]}")
+            msg_len = bytes(msg[:HEADERSIZE])
+            new_msg = False
 
-		full_msg += msg.decode("utf-8")
-		if len(full_msg)-HEADERSIZE == msg_len:
-			print("Full msg recvd !")
-			print(full_msg[HEADERSIZE:])
-			new_msg = True
-			full_msg = ""
+        full_msg += msg.decode("utf-8")
+        if len(full_msg)-HEADERSIZE == msg_len:
+            print("Full msg recvd !")
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ""
+    print(full_msg)
 
-	print(full_msg)
--- data_buff/cli_serv_buff/server_a.py
+++ data_buff/cli_serv_buff/server_a.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 import socket
+import time
 
 HEADERSIZE = 10
 
+# No good socket for sending memoryview !!!
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1235))
 s.listen(5)
@@ -18,7 +20,13 @@ while True:
     clientsocket.send(bytes(msg, "utf-8"))
 
     while True:
-        time.sleep(3)
-        msg = f"Time is : "{time.time()}
+        #time.sleep(3)
+        #msg = f"Time is : {date.time()}"
+        data = b"a" #* (1024 * 100000)
+        mv = memoryview(data)
+        #sent = s.send(mv)
+        #mv = mv[sent:]
         msg = f"{len(msg):<{HEADERSIZE}}" + msg
         clientsocket.send(bytes(msg, "utf-8"))
+        sent = clientsocket.send(mv)
+        mv = mv[sent:]
